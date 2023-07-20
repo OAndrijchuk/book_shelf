fe4e54b9087367d5495b0a4c3955ce3522c2517e
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-07-20 18:31:40 +0300

    ф

--- src/scripts/shoping-list-markup.js
+++ src/scripts/shoping-list-markup.js
@@ -1,4 +1,4 @@
-import iconsSL from '../images/sprite.svg'
+import iconsSL from '../images/sprite.svg';
 import emptyImage from '../images/empty-cont-img.png';
 
 export function createBookMarkup(object) {
@@ -37,11 +37,11 @@ export function createBookMarkup(object) {
       
        <div class="shopping-list-book-links">
 
-      <a href="${amazon.url}"><svg width="32" height="11" class="shopping-list-amazon">
+    <a class="test-link" href="${amazon.url}"><svg width="32" height="11" class="shopping-list-amazon">
       <use href="${iconsSL}#icon-amazon"></use>
-    </svg></a>
-       <a href="${apple_books.url}"><svg width="16" height="16" class="shopping-list-apple"><use href="${iconsSL}#icon-apple-books-logo"></use></svg></a>
-       <a href="${bookshop.url}"><svg width="16" height="16" class="shopping-list-bookshop"><use href="${iconsSL}#icon-book-shops-logo"></use></svg></a>
+      </svg></a>
+    <a class="test-link" href="${apple_books.url}"><svg   width="16" height="16" class="shopping-list-apple"><use href="${iconsSL}#icon-apple-books-logo"></use></svg></a>
+    <a class="test-link" href="${bookshop.url}"><svg width="16" height="16" class="shopping-list-bookshop"><use href="${iconsSL}#icon-book-shops-logo"></use></svg></a>
        </div>
       <button class="shopping-list-book-btn">
       <svg class="shopping-list-book-btn-icon" width="16" height="16"><use href="${iconsSL}#icon-bag"></use></svg></button>
@@ -58,27 +58,27 @@ export function createBookMarkup(object) {
 //       <a href="${apple_books.url}"></a>
 //       <a href="${bookshop.url}"></a>
 export function createShopingList(arrayOfBooks) {
-  let result 
+  let result;
   if (arrayOfBooks.length === 0) {
-    result = createEmptyBackground()
+    result = createEmptyBackground();
   } else {
     const mappedBooks = arrayOfBooks.join('');
-  result = `
-  <div class="container">
+    result = `
+  <div class="container grid-wrapper shopping-list-container">
     <h2 class="shop-list-title">Shopping <span class="shop-list-title-span">List</span></h2>
     <ul class="shopping-list">${mappedBooks}</ul>
-  </div>`
+  </div>`;
   }
   return result;
 }
 
-export function createEmptyBackground () {
-  return  `
-    <div class="container">
+export function createEmptyBackground() {
+  return `
+    <div class="container grid-wrapper shopping-list-container">
       <h2 class="shop-list-title">Shopping <span class="shop-list-title-span">List</span></h2>
       <div class="shop-list-empty-container">
         <p class="shop-list-empty-paragraph">This page is empty, add some books and proceed to order.</p>
         <img class="shop-list-empty-img" src="${emptyImage}" alt="Books in empty section"/>
       </div>
-    </div>`
+    </div>`;
 }
