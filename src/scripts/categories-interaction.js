4f4eacba17c8b52b852ee9e5638aa4c797d55c5a
Taras Kovalyk <tarik2454@gmail.com>
2023-07-20 17:08:21 +0300

    fix

--- src/scripts/categories-interaction.js
+++ src/scripts/categories-interaction.js
@@ -1,13 +1,19 @@
 import { FetchBook } from './api';
 import { createCategorieTpl } from './categorie-tpl';
 import { createCardTpl } from './card-tpl';
+import { updateScreenWidth } from './best-books';
 
 const asideList = document.querySelector('.aside-list');
 const container = document.querySelector('.cont-section');
+const categoriesListName = document.querySelector('.categories-list-name');
 
 const fetchBook = new FetchBook();
 
 asideList.addEventListener('click', onCategoryClick);
+categoriesListName.addEventListener('click', () => {
+  // container.innerHTML = '';
+  updateScreenWidth();
+});
 
 function createBookCategogies() {
   fetchBook
@@ -17,6 +23,9 @@ function createBookCategogies() {
         .map(categorie => createCategorieTpl(categorie))
         .join('');
       createCategogiesMarkup(categoryRender);
+
+      const selector = document.querySelectorAll('.aside-link');
+      changeColoursCategories(selector);
     })
     .catch(error => {
       console.warn(error);
@@ -33,6 +42,7 @@ function onCategoryClick({ target }) {
   if (!target.classList.contains('aside-link')) {
     return;
   }
+
   const searchQuery = target.textContent;
 
   chooseCategory(searchQuery);
@@ -66,4 +76,28 @@ function createCardMarkup(listMap, searchQuery) {
   <h1 class="books-section-title">${highlightedQuery}</h1>
   <ul class="card-list">${listMap}</ul>
   </div>`;
+}
+
+function changeColoursCategories(selector) {
+  selector.forEach(item => {
+    item.addEventListener('click', () => {
+      selector.forEach(link => {
+        categoriesListName.classList.remove('activ-name');
+
+        if (link === item) {
+          link.classList.add('activ-name');
+        } else {
+          link.classList.remove('activ-name');
+        }
+      });
+    });
+  });
+
+  categoriesListName.addEventListener('click', () => {
+    selector.forEach(link => {
+      link.classList.remove('activ-name');
+    });
+
+    categoriesListName.classList.add('activ-name');
+  });
 }
