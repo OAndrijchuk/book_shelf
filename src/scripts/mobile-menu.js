a40e7daf92655a598832d93e46ba6d47c668b0dc
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-07-20 19:30:12 +0300

    123
    +++

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -6,7 +6,7 @@ const closeIco = document.querySelector('.close-menu-icon-cont');
 burgerBtn.addEventListener('click', showMobileMenu);
 
 function showMobileMenu(event) {
-  document.body.classList.toggle('no-scroll');
+  document.body.classList.toggle('modal-open');
   burgerIco.classList.toggle('visually-hidden');
   closeIco.classList.toggle('visually-hidden');
   mobileMenu.classList.toggle('is-open');
