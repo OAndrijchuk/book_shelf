6d26f55fbd961f7d4c8d21859a55032941de860c
Olga-Olga <olga_l@ukr.net>
2023-07-23 15:52:33 +0300

    git add .

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -5,3 +5,21 @@ menuLinks[0].classList.add('activ-page');
 const mobileLinks = document.querySelectorAll('.mob-menu-link');
 mobileLinks.forEach(el => el.classList.remove('activ-page'));
 mobileLinks[0].classList.add('activ-page');
+
+const userEl = document.querySelector(".user-btn")
+const logOutBtn = document.querySelector(".sign-out-btn");
+const signUpBtn = document.querySelector(".sign-up-btn")
+
+userEl.addEventListener("click", showLogOut)
+function showLogOut() {   
+    logOutBtn.classList.toggle("hidden-button")
+}
+
+logOutBtn.addEventListener("click", logOutUserSecssion)
+function logOutUserSecssion() {   
+    logOutBtn.classList.toggle("hidden-button")
+    userEl.classList.add("hide-user")
+    signUpBtn.classList.remove("hidden-button")
+    // !NEED to add LOGIC TO "LOG OUT" or Redirect to "Log Out page"
+}
+
